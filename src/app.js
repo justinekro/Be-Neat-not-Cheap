5a91eda204a70e6936213bb0e3acfd1e7b410bbe
justinekro <justinekronovsek@gmail.com>
2018-06-05 13:50:16 +0200

    get firebase expenses when starting app

--- src/app.js
+++ src/app.js
@@ -5,20 +5,13 @@ import AppRouter from './routers/app_router';
 import configureStore from './store/configure_store';
 import 'normalize.css/normalize.css'; // cross browser setting
 import './styles/styles.scss';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import './firebase/firebase';
 import './playground/promises'
 
 const store = configureStore();
-
-// store.dispatch(addExpense({description:'water bill',amount: 3000, createdAt: 1000}))
-// store.dispatch(addExpense({description:'gas bill',amount:2000, createdAt: 200}))
-// store.dispatch(addExpense({description:'rent',amount:1000, createdAt: 3000}))
-//
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 
 const jsx = (
   <Provider store={store}>
@@ -27,5 +20,12 @@ const jsx = (
 )
 
 ReactDOM.render((
-  jsx
+  <p>Loading...</p>
 ), document.getElementById('app'))
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render((
+    jsx
+  ), document.getElementById('app'))
+
+})
